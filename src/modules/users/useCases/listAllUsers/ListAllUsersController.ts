c5993777e1e9dffe76403f13ebe0cd3489c92099
eduardomausa <mausaeduardo@gmail.com>
2022-05-04 21:10:43 -0300

    Challenge finished!

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const listOfUsers = this.listAllUsersUseCase.execute({ user_id } as {
+        user_id: string;
+      });
+      return response.status(200).json(listOfUsers);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
